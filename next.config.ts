dbd554eafd8b7f1a39483536bbe5763a0309d1a7
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-15 17:49:58 +0900

    init commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+    return config;
+  }
 };
 
 export default nextConfig;
