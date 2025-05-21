be1881a66d3dffc9d9e220b729b7744d7df2edf4
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-21 17:28:29 +0900

    feat:add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from 'next/app';
-import Head from "next/head";
+import Head from 'next/head';
 import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-
                 <title>Memo-App</title>
                 <meta name="description" content="A simple Next.js application" />
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
+
             <Component {...pageProps} />
         </>
     );
