fcc6d5394765605140c8a45e6993a7a476f20c42
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-22 15:03:20 +0900

    fix:add registerbutton for header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useHeaderStore } from "@/store/userHeaderStore";
 import { useRouter } from "next/navigation";
-import { LogIn } from "lucide-react";
+import { LogIn, UserPlus } from "lucide-react";
 
 export default function Header() {
     const isLoggedIn = useHeaderStore((state) => state.isLoggedIn);
@@ -10,6 +10,10 @@ export default function Header() {
 
     const handleLogin = () => {
         router.push("/login");
+    };
+
+    const handleSignUp = () => {
+        router.push("/register");
     };
 
     return (
@@ -28,14 +32,24 @@ export default function Header() {
                     Memo App
                 </h1>
             </div>
+
             {!isLoggedIn && (
-                <Button
-                    onClick={handleLogin}
-                    className="flex items-center gap-2 bg-white text-blue-700 font-semibold px-6 py-2 rounded-xl shadow hover:bg-blue-50 transition cursor-pointer"
-                >
-                    <LogIn className="w-5 h-5" />
-                    ログイン
-                </Button>
+                <div className="flex gap-4">
+                    <Button
+                        onClick={handleLogin}
+                        className="flex items-center gap-2 bg-white text-blue-700 font-semibold px-6 py-2 rounded-xl shadow hover:bg-blue-50 transition cursor-pointer"
+                    >
+                        <LogIn className="w-5 h-5" />
+                        ログイン
+                    </Button>
+                    <Button
+                        onClick={handleSignUp}
+                        className="flex items-center gap-2 bg-yellow-50 text-yellow-800 font-semibold px-6 py-2 rounded-xl shadow hover:bg-yellow-100 transition cursor-pointer"
+                    >
+                        <UserPlus className="w-5 h-5" />
+                        新規登録
+                    </Button>
+                </div>
             )}
         </header>
     );
